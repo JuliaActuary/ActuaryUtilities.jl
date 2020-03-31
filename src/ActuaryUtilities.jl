ec7f0511e226885dbd82ff5ea3760b47af787d10
Alec Loudenback <alecloudenback@gmail.com>
2020-03-30 21:00:08 -0500

    add years_between()

--- src/ActuaryUtilities.jl
+++ src/ActuaryUtilities.jl
@@ -1,5 +1,54 @@
 module ActuaryUtilities
 
-greet() = print("Hello World!")
+using Dates
+
+
+
+"""
+    Years_Between(Date,Date)
+    
+    Compute the number of integer years between two dates, with the 
+    first date typically before the second. Will return negative number if
+    first date is after the second. Use third argument to indicate if calendar 
+    annivesary should count as a full year.
+
+# Examples
+```jldoctest
+julia> d1 = Date(2018,09,30)
+julia> d2 = Date(2019,09,30)
+julia> d3 = Date(2019,10,01)
+
+julia> years_between(d1,d3) 
+1
+julia> years_between(d1,d2,false) # same month/day but `false` overlap
+0 
+julia> years_between(d1,d2) # same month/day but `true` overlap
+1 
+julia> years_between(d1,d2) # using default `true` overlap
+1 
+```
+"""
+function years_between(d1::Date,d2::Date,overlap=true)
+    iy,im,id = Dates.year(d1), Dates.month(d1), Dates.day(d1)
+    vy,vm,vd = Dates.year(d2), Dates.month(d2), Dates.day(d2)
+    dur = vy - iy
+    if vm == im
+        if overlap
+            if vd >= id
+                 dur += 1
+            end
+        else
+            if vd > id
+                 dur += 1
+            end
+        end
+    elseif vm > im
+        dur += 1
+    end
+
+    return dur - 1
+end
+
+export years_between
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using ActuaryUtilities
+
+using Dates
 using Test
 
-@testset "ActuaryUtilities.jl" begin
-    # Write your own tests here.
+@testset "Temporal functions" begin
+    @testset "years_between" begin
+        @test years_between(Date(2018,9,30),Date(2018,9,30)) == 0
+        @test years_between(Date(2018,9,30),Date(2018,9,30),true) == 0
+        @test years_between(Date(2018,9,30),Date(2019,9,30),false) == 0
+        @test years_between(Date(2018,9,30),Date(2019,9,30),true) == 1
+        @test years_between(Date(2018,9,30),Date(2019,10,1),true) == 1
+        @test years_between(Date(2018,9,30),Date(2019,10,1),false) == 1
+    end
+
 end
