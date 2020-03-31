388be65595d2a14b84f4c35dd0db31b7a8bcb171
Alec Loudenback <alecloudenback@gmail.com>
2020-03-30 23:25:08 -0500

    remove doctests from normal runtests - they should run during doc build

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,7 @@
 using ActuaryUtilities
 
 using Dates
-using Documenter
 using Test
-
-# run doctests
-doctest(ActuaryUtilities)
 
 @testset "Temporal functions" begin
     @testset "years_between" begin
