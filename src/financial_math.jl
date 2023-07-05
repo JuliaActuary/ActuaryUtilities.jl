fe5cfdae5738b7ae3b1b1e11bfa2ccaf3fb17169
Alec Loudenback <alecloudenback@gmail.com>
2023-07-04 22:07:18 -0500

    add `spread` example

--- src/financial_math.jl
+++ src/financial_math.jl
@@ -410,6 +410,13 @@ end
     spread(curve1,curve2,cashflows)
 
 Return the solved-for constant spread to add to `curve1` in order to equate the discounted `cashflows` with `curve2`
+
+# Examples
+
+```julia-repl
+spread(0.04, 0.05, cfs)
+Rate{Float64, Periodic}(0.010000000000000009, Periodic(1))
+```
 """
 function spread(curve1, curve2, cashflows, times=eachindex(cashflows))
     times = FinanceCore.timepoint.(cashflows, times)
