fdaab14bf82b710bc855c3f80b5e13accfec2226
alecloudenback <alecloudenback@users.noreply.github.com>
2023-09-04 23:12:55 -0500

    re-enable precompile (#103)

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,43 +1,42 @@
 
-# # created with the help of SnoopCompile.jl
-# @setup_workload begin
-#     # Putting some things in `setup` can reduce the size of the
-#     # precompile file and potentially make loading faster.
-#     cfs = [10 for i in 1:10]
-    
-#     # 2021-03-31 rates from Treasury.gov
-#     rates =[0.01, 0.01, 0.03, 0.05, 0.07, 0.16, 0.35, 0.92, 1.40, 1.74, 2.31, 2.41] ./ 100
-#     mats = [1/12, 2/12, 3/12, 6/12, 1, 2, 3, 5, 7, 10, 20, 30]
+# created with the help of SnoopCompile.jl
+@setup_workload begin
+    # Putting some things in `setup` can reduce the size of the
+    # precompile file and potentially make loading faster.
+    cfs = [10 for i in 1:10]
 
-#     y = Yields.CMT(rates,mats)
-#     r = 0.05
-#     rates = [r,y]
-#     @compile_workload begin
-#         # all calls in this block will be precompiled, regardless of whether
-#         # they belong to your package or not (on Julia 1.8 and higher)
-#         irr([-80;cfs])
-#         moic([-80;cfs])
+    # 2021-03-31 rates from Treasury.gov
+    rates = [0.01, 0.01, 0.03, 0.05, 0.07, 0.16, 0.35, 0.92, 1.40, 1.74, 2.31, 2.41] ./ 100
+    mats = [1 / 12, 2 / 12, 3 / 12, 6 / 12, 1, 2, 3, 5, 7, 10, 20, 30]
 
-#         for v in rates
-#             pv(v,cfs)
-#             duration(v,cfs)
-#             convexity(v,cfs)
-#             duration(Macaulay(),v,cfs)
-#             duration(DV01(),v,cfs)
-#             duration(Modified(),v,cfs)
-#             # duration(KeyRate(5),v,cfs)
+    y = FinanceModels.fit(FinanceModels.Spline.Linear(), CMTYield.(rates, mats), FinanceModels.Fit.Bootstrap())
+    r = 0.05
+    rates = [r, y]
+    @compile_workload begin
+        # all calls in this block will be precompiled, regardless of whether
+        # they belong to your package or not (on Julia 1.8 and higher)
+        moic([-80; cfs])
 
-#             breakeven(v, [-10,1,2,3,4,8])
+        for v in rates
+            pv(v, cfs)
+            duration(v, cfs)
+            convexity(v, cfs)
+            duration(Macaulay(), v, cfs)
+            duration(DV01(), v, cfs)
+            duration(Modified(), v, cfs)
+            # duration(KeyRate(5),v,cfs)
 
-#             spread(v - .01,v,cfs)
+            breakeven(v, [-10, 1, 2, 3, 4, 8])
 
-#         end
-#         spread(Yields.Continuous(0.04),Yields.Continuous(0.05),cfs)
+            spread(v - 0.01, v, cfs)
 
+        end
+        spread(y, y + 0.01, cfs)
 
 
-#         years_between(Date(2018, 9, 30), Date(2018, 9, 30))
-#         duration(Date(2018, 9, 30), Date(2019, 9, 30))
 
-#     end
-# end
+        years_between(Date(2018, 9, 30), Date(2018, 9, 30))
+        duration(Date(2018, 9, 30), Date(2019, 9, 30))
+
+    end
+end
